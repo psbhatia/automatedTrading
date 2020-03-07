d85334a8e3c678c34a296f659ed89e5ea1a34ab5
psbhatia <psbhatia@uwaterloo.ca>
2020-03-07 17:33:02 -0500

    Can now get API response in string format

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,10 +17,10 @@ public class Main {
         String APIkey = "A5ETHRCZN7N9TV4K";
         String baseURL = "https://www.alphavantage.co/query?function=%&symbol=%&interval=%min&apikey=%";
 
-        String urlString = buildUrl(baseURL, "TestFunction","testSymbol", 5, APIkey);
+        String urlString = buildUrl(baseURL, "TIME_SERIES_INTRADAY","AAPL", 5, APIkey);
         try {
-            int responseCode = sendGETRequest(urlString);
-            System.out.println(responseCode);
+            String response = sendGETRequest(urlString);
+            System.out.println(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,11 +39,20 @@ public class Main {
     /*
     Make a get request to the provided URL
      */
-    private static int sendGETRequest(String urlString) throws IOException {
+    private static String sendGETRequest(String urlString) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        return connection.getResponseCode();
+//        int responseCode = connection.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuffer content = new StringBuffer();
+        String inputLine;
+        while ((inputLine=in.readLine())!=null){
+            content.append(inputLine);
+        }
+        in.close();
+
+        return content.toString();
     }
 
 }
