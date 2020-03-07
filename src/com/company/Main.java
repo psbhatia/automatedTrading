75239cbcbc767af8d1e69b11d34a630c8f37d3ef
psbhatia <psbhatia@uwaterloo.ca>
2020-03-07 15:58:50 -0500

    can create URL to send HTTP request to

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
 
+    public final String APIKey = "A5ETHRCZN7N9TV4K";
+    private final String baseURL = "https://www.alphavantage.co/query?function=%&symbol=%&interval=%min&apikey=%";
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+
+        String APIkey = "A5ETHRCZN7N9TV4K";
+        String baseURL = "https://www.alphavantage.co/query?function=%&symbol=%&interval=%min&apikey=%";
+
+        String url = buildUrl(baseURL, "TestFunction","testSymbol", 5, APIkey);
+        System.out.println(url);
+    }
+
+    /*
+    Build the API request to get the INTRADAY time series
+     */
+    private static String buildUrl(String baseURL, String function, String symbol, int interval, String APIKey){
+        String url = baseURL.replaceFirst("%", function).replaceFirst("%",symbol).replaceFirst("%",String.valueOf(interval)).replaceFirst("%",APIKey);
+        return url;
     }
 }
